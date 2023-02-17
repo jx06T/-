d38188cc6cae4cb03bf187230b1deb6dab09073e
jx06T <119775148+jx06T@users.noreply.github.com>
2023-02-17 18:40:33 +0800

    Update remote_control.py

--- 點歌/remote_control.py
+++ 點歌/remote_control.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import pprint
 import time
 url = "https://script.google.com/macros/s/AKfycbwgrFPmi7kzaXDQlZ0IIkjsW1LFMLpBydb_bvOVhXfgKT1nLl28bO0fznAIqjpKw6RE1Q/exec"
 
@@ -13,7 +12,7 @@ def POST():
         "return": "T",
         "menu":[["1abc","2bcd","3qwe",4,5,6,7,8,9,10]]}
     response = requests.post(url, data=json.dumps(payload))
-    pprint.pprint(response.json())
+    print(response.json())
 
 while 1==1:
     time.sleep(1)
