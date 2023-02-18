a2cf297fdadb86814442dd08f7cb3c456c127309
jx06T <119775148+jx06T@users.noreply.github.com>
2023-02-18 19:16:49 +0800

    Update js.js

--- 點歌/js.js
+++ 點歌/js.js
@@ -2,6 +2,8 @@ let url = 'https://script.google.com/macros/s/AKfycbwgrFPmi7kzaXDQlZ0IIkjsW1LFML
 let type = "u"
 let N = ""
 let K = 0
+let Nowtime = 166
+let Alltime = 503
 let getJSON = async (url, k, v, m, n) => {
     console.log(url + `?k=${k}&v=${v}&m=${m}&n=${n}`)
     let response = await fetch(url + `?k=${k}&v=${v}&m=${m}&n=${n}`);
@@ -13,32 +15,53 @@ let getJSON = async (url, k, v, m, n) => {
 
 function updata(data) {
     document.getElementById("name").innerText = data[0]
-    document.getElementById("time").innerText = data[2] + "/" + data[1]
-    let time1 = (data[2][0].split(":")[0] * 60) + (data[2][0].split(":")[1])
-    let time2 = (data[1][0].split(":")[0] * 60) + (data[1][0].split(":")[1])
+    if (Alltime != Number(data[1][0].split(":")[0] * 60) + Number(data[1][0].split(":")[1])) {
+        document.getElementById("time").innerText = data[2] + "/" + data[1]
+        Nowtime = Number(data[2][0].split(":")[0] * 60) + Number(data[2][0].split(":")[1])
+        Alltime = Number(data[1][0].split(":")[0] * 60) + Number(data[1][0].split(":")[1])
+        document.getElementById("schedule").value = Nowtime / Alltime * 100    
+    }
     document.getElementById("s1").innerText = data[3][0]
     document.getElementById("s2").innerText = data[3][1]
     document.getElementById("s3").innerText = data[3][2]
-    document.getElementById("schedule").value = time1 / time2 * 100
 
 }
 function check(id) {
     if (type != "u") return
     type = "c"
     N = id
+    getJSON(url, K, volume.value, type, N)
 }
 function search() {
     type = "s"
     N = keyword.value
+    getJSON(url, K, volume.value, type, N)
 }
+function PrintTime(n, a) {
+    output = ""
+    if (Math.floor(n / 60) < 10) output += "0"
+    output += Math.floor(n / 60)
+    output += ":"
+    if (Math.floor(n % 60 < 10)) output += "0"
+    output += Math.floor(n % 60)
+    output += "/"
+    if (Math.floor(a / 60) < 10) output += "0"
+    output += Math.floor(a / 60)
+    output += ":"
+    if (Math.floor(a % 60) < 10) output += "0"
+    output += Math.floor(a % 60)
+    return output
+}
+
 document.getElementById("sand").onclick = () => search()
 document.getElementById("play").onclick = () => {
     K = (K + 1) % 2
     if (K % 2 == 1) {
         document.getElementById("play").className = "gg-play-button-o"
-    }else{
+    } else {
         document.getElementById("play").className = "gg-play-pause-o"
     }
+    getJSON(url, K, volume.value, type, N)
 }
 document.getElementById("s1").onclick = () => check(1)
 document.getElementById("s2").onclick = () => check(2)
@@ -46,8 +69,13 @@ document.getElementById("s3").onclick = () => check(3)
 const keyword = document.getElementById("in");
 const volume = document.getElementById("volume");
 
+getJSON(url, K, volume.value, type, N)
+
 setInterval(function () {
     // console.log(type)
-    getJSON(url, K, volume.value, type, N)
-    type = "u"
-}, 1500);
+    if (K == 0) {
+        Nowtime += 0.334
+    }
+    document.getElementById("schedule").value = Nowtime / Alltime * 100
+    document.getElementById("time").innerText = PrintTime(Nowtime, Alltime)
+}, 333);
